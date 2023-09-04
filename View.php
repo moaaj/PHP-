514b5f19a07ff449700e9d8ba040255db855dc29
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2023-09-04 12:48:06 +0800

    Add files via upload

---
+++ View.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+$name=$_SESSION['Name'];
+$age=$_SESSION['Age'];
+$weight=$_SESSION['Weight'];
+
+echo $name.'<br>';
+echo $age.'<br>';
+echo $weight.'<br>';
+
+
+
+
+
+
+
+
+?>
